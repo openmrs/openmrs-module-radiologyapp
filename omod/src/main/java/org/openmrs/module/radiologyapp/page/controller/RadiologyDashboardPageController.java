3563e72a51a8ca6daf44a71c14f38f7e1759ec32
Cosmin Ioan <cioan@pih.org>
2015-08-27 15:28:04 -0400

    UHM-1991, add page to display only the radiology encounters

---
+++ omod/src/main/java/org/openmrs/module/radiologyapp/page/controller/RadiologyDashboardPageController.java
@@ -0,0 +1,70 @@
+package org.openmrs.module.radiologyapp.page.controller;
+
+
+import org.openmrs.Location;
+import org.openmrs.Patient;
+import org.openmrs.api.OrderService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.appframework.context.AppContextModel;
+import org.openmrs.module.appframework.service.AppFrameworkService;
+import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.module.coreapps.CoreAppsConstants;
+import org.openmrs.module.coreapps.CoreAppsProperties;
+import org.openmrs.module.coreapps.contextmodel.PatientContextModel;
+import org.openmrs.module.coreapps.contextmodel.VisitContextModel;
+import org.openmrs.module.emrapi.adt.AdtService;
+import org.openmrs.module.emrapi.event.ApplicationEventService;
+import org.openmrs.module.emrapi.patient.PatientDomainWrapper;
+import org.openmrs.module.emrapi.visit.VisitDomainWrapper;
+import org.openmrs.ui.framework.annotation.InjectBeans;
+import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.ui.framework.page.Redirect;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class RadiologyDashboardPageController {
+    public Object controller(@RequestParam("patientId") Patient patient,
+                             PageModel model,
+                             @InjectBeans PatientDomainWrapper patientDomainWrapper,
+                             @SpringBean("adtService") AdtService adtService,
+                             @SpringBean("coreAppsProperties") CoreAppsProperties coreAppsProperties,
+                             @SpringBean("applicationEventService") ApplicationEventService applicationEventService,
+                             UiSessionContext sessionContext) {
+
+        if (!Context.hasPrivilege(CoreAppsConstants.PRIVILEGE_PATIENT_VISITS)) {
+            return new Redirect("coreapps", "noAccess", "");
+        }
+        else if (patient.isVoided() || patient.isPersonVoided()) {
+            return new Redirect("coreapps", "patientdashboard/deletedPatient", "patientId=" + patient.getId());
+        }
+
+        patientDomainWrapper.setPatient(patient);
+        model.addAttribute("patient", patientDomainWrapper);
+
+
+        Location visitLocation = null;
+        try {
+            visitLocation = adtService.getLocationThatSupportsVisits(sessionContext.getSessionLocation());
+        } catch (IllegalArgumentException ex) {
+            // location does not support visits
+        }
+
+        VisitDomainWrapper activeVisit = null;
+        if (visitLocation != null) {
+            activeVisit = adtService.getActiveVisit(patient, visitLocation);
+        }
+        model.addAttribute("activeVisit", activeVisit);
+
+        AppContextModel contextModel = sessionContext.generateAppContextModel();
+        contextModel.put("patient", new PatientContextModel(patient));
+        contextModel.put("visit", activeVisit == null ? null : new VisitContextModel(activeVisit));
+        model.addAttribute("appContextModel", contextModel);
+
+        model.addAttribute("dashboardUrl", coreAppsProperties.getDashboardUrl());
+
+        applicationEventService.patientViewed(patient, sessionContext.getCurrentUser());
+
+        return null;
+    }
+
+}
---
+++ omod/src/main/webapp/pages/radiologyDashboard.gsp
@@ -0,0 +1,36 @@
+<%
+    // although called "patientDashboard" this is actually the patient visits screen, and clinicianfacing/patient is the main patient dashboard
+
+    ui.decorateWith("appui", "standardEmrPage")
+
+    ui.includeCss("coreapps", "patientdashboard/patientDashboard.css")
+
+    ui.includeJavascript("uicommons", "bootstrap-collapse.js")
+    ui.includeJavascript("uicommons", "bootstrap-transition.js")
+
+
+%>
+<script type="text/javascript">
+    var breadcrumbs = [
+        { icon: "icon-home", link: '/' + OPENMRS_CONTEXT_PATH + '/index.htm' },
+        { label: "${ ui.escapeJs(ui.format(patient.patient)) }" ,
+            link: '${ ui.urlBind("/" + contextPath + dashboardUrl, [ patientId: patient.patient.id ] ) }'},
+        { label: "${ ui.message("coreapps.clinicianfacing.radiology") }",
+            link: "${ ui.pageLink("coreapps", "patientdashboard/radiologyDashboard", [app: "appointmentschedulingui.schedulingAppointmentApp"]) }" }
+    ];
+
+    jq(function(){
+        // make sure we reload the page if the location is changes; this custom event is emitted by by the location selector in the header
+        jq(document).on('sessionLocationChanged', function() {
+            window.location.reload();
+        });
+    });
+
+    var patient = { id: ${ patient.id } };
+</script>
+
+
+${ ui.includeFragment("coreapps", "patientHeader", [ patient: patient.patient, activeVisit: activeVisit, appContextModel: appContextModel ]) }
+<div id="radiologyapp-id">
+    ${ ui.includeFragment("radiologyapp", "radiologyTab", [ patient: patient ]) }
+</div>
